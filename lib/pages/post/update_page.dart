19380eb44852c727675e41fe9fe944c32f8c24e5
mugon-dev <jmg41490400@gmail.com>
2021-07-15 19:56:20 +0900

    삭제, 수정 버튼 및 페이지 이동 적용

--- lib/pages/post/update_page.dart
+++ lib/pages/post/update_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class UpdatePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
